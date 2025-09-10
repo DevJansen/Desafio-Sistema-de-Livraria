a2d7546383df957cf2045c3983b4954dd5db2a8d
Jhonata Jansen <codejhonatajansen@gmail.com>
2025-09-10 09:16:07 -0300

    criando a classe autor

---
+++ Autor.java
@@ -0,0 +1,47 @@
+import javax.xml.crypto.Data;
+
+public class Autor {
+
+    private int id = 0;
+    private String nome;
+    private Data dataNascimento;
+
+    public Autor(int id, String nome, Data dataNascimento){
+        this.id = id++;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Data getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Data dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                '}';
+    }
+}
